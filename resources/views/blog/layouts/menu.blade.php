876eb443dcd1abaabf27a2883a9ccdcefda47d2a
Dian Pratama <dianpratama097@gmail.com>
2023-04-19 21:16:02 +0700

    update login

--- resources/views/blog/layouts/menu.blade.php
+++ resources/views/blog/layouts/menu.blade.php
@@ -1,48 +1,44 @@
 @extends('blog.layouts.template_home')
 @section('content')
-    <div class="d-flex justify-content-center">
-        <div class="card" style="width: 50%">
-            <div class="card-body">
-                <div class="mb-5">
-                    <form method="POST" action="{{ route('login') }}"> @csrf
-                        <div class="modal-body">
-                            <div class="form-group">
-                                <label for="username">NISN</label>
-                                <input id="username" type="text"
-                                    class="form-control @error('username') is-invalid @enderror" name="username"
-                                    value="{{ old('username') }}" required autocomplete="username" autofocus>
-                                @error('username')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input id="password" name="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password" required
-                                    autocomplete="current-password">
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+    <div class="wrapper">
+        <div class="logo">
+            <img src="{{ asset('assets/img/smk.png') }}"
+                alt="smk">
+        </div>
+        <div class="text-center mt-4 name">
+            LOGIN
+        </div>
 
-                        </div>
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-xs btn-block muda">Masuk</button>
-                        </div>
-                    </form>
-                </div>
+        <form method="POST" action="{{ route('login') }}" class="p-3 mt-3"> @csrf
+            <div class="form-field d-flex align-items-center">
+                <input id="username" type="text" class=" @error('username') is-invalid @enderror" name="username"
+                    value="{{ old('username') }}" required autocomplete="username" autofocus placeholder="Username/NISN">
+                @error('username')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <div class="form-field d-flex align-items-center">
+                <input id="password" name="password" type="password" class="@error('password') is-invalid @enderror"
+                    name="password" required autocomplete="current-password" placeholder="Password">
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+            <button class="btn mt-3">Login</button>
+        </form>
+        <div class="text-center fs-6">
+            <div class="d-flex justify-content-center">
+                <a href="/" style="text-decoration:none;">
+                    <div
+                        style="background: rgb(109, 197, 255); color: rgb(255, 255, 255); border-radius: 10px; margin: 3px;">
+                        <h4 class="card-title text-center mt-2 huruf" style="padding: 5px">My School</h4>
+                    </div>
+                </a>
             </div>
         </div>
-    </div>
-    <div class="d-flex justify-content-center">
-        <a href="/" style="text-decoration:none;">
-            <div style="background: rgb(109, 197, 255); color: rgb(255, 255, 255); border-radius: 10px; margin: 5px;">
-                <h4 class="card-title text-center mt-3 huruf" style="padding: 10px">My School</h4>
-            </div>
-        </a>
     </div>
 @endsection
--- resources/views/blog/layouts/template_home.blade.php
+++ resources/views/blog/layouts/template_home.blade.php
@@ -13,44 +13,121 @@
     <title>SMK Negeri 1 Singkep</title>
 
     <style type="text/css">
-        .huruf {
-            font-family: cursive;
+        /* Importing fonts from Google */
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap');
+
+        /* Reseting */
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Poppins', sans-serif;
         }
 
-        .kuning {
-            background-color: rgb(255, 205, 40);
-            color: rgb(255, 255, 255)
+        body {
+            background: #ecf0f3;
         }
 
-        .merah {
-            background-color: rgb(245, 0, 94);
-            color: rgb(255, 255, 255)
+        .wrapper {
+            
+            max-width: 350px;
+            min-height: 500px;
+            margin: 100px auto;
+            padding: 40px 30px 30px 30px;
+            background-color: #ecf0f3;
+            border-radius: 15px;
+            box-shadow: 13px 13px 20px #cbced1, -13px -13px 20px #fff;
         }
 
-        .muda {
-            background-color: rgb(134, 225, 255);
-            color: rgb(255, 255, 255)
+        .logo {
+            width: 70px;
+            margin: auto;
         }
-        .hijau {
-            background-color: rgb(10, 255, 22);
-            color: rgb(255, 255, 255)
+
+        .logo img {
+            width: 100%;
+            height: 80px;
+            object-fit: cover;
+            border-radius: 50%;
+            box-shadow: 0px 0px 3px #5f5f5f,
+                0px 0px 0px 5px #ecf0f3,
+                8px 8px 15px #a7aaa7,
+                -8px -8px 15px #fff;
+        }
+
+        .wrapper .name {
+            font-weight: 600;
+            font-size: 1.2rem;
+            letter-spacing: 1.3px;
+            padding-left: 10px;
+            color: #555;
+        }
+
+        .wrapper .form-field input {
+            width: 100%;
+            display: block;
+            border: none;
+            outline: none;
+            background: none;
+            font-size: 1.2rem;
+            color: #666;
+            padding: 10px 15px 10px 10px;
+            /* border: 1px solid red; */
+        }
+
+        .wrapper .form-field {
+            padding-left: 10px;
+            margin-bottom: 20px;
+            border-radius: 20px;
+            box-shadow: inset 8px 8px 8px #cbced1, inset -8px -8px 8px #fff;
+        }
+
+        .wrapper .form-field .fas {
+            color: #555;
+        }
+
+        .wrapper .btn {
+            box-shadow: none;
+            width: 100%;
+            height: 40px;
+            background-color: #03A9F4;
+            color: #fff;
+            border-radius: 25px;
+            box-shadow: 3px 3px 3px #b1b1b1,
+                -3px -3px 3px #fff;
+            letter-spacing: 1.3px;
+        }
+
+        .wrapper .btn:hover {
+            background-color: #039BE5;
+        }
+
+        .wrapper a {
+            text-decoration: none;
+            font-size: 0.8rem;
+            color: #03A9F4;
+        }
+
+        .wrapper a:hover {
+            color: #039BE5;
+        }
+
+        @media(max-width: 380px) {
+            .wrapper {
+                margin: 30px 20px;
+                padding: 40px 15px 15px 15px;
+            }
         }
     </style>
-    <link rel="stylesheet" href="{{ asset('home/css/style.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('home/css/style.css') }}"> --}}
 </head>
 
-<body style="background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(47,128,237,1) 0%, rgba(84,197,233,1) 100%);">
+<body>
     <div class="container" style="margin-top: 3%">
-        <h2 class="text-center mb-4 huruf" style="color: rgb(255, 255, 255); ">SMK Negeri 1 Singkep</h2>
+        {{-- <u><h2 class="text-center" style="color: rgb(17, 5, 70); ">SMK Negeri 1 Singkep</h2></u> --}}
         @yield('content')
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
-    </script>
 </body>
 
 </html>
